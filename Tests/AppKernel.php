3a792ace7c1bfcd61113d91b74c3a5e689c121e9
Nicolas Claverie <info@artscore-studio.fr>
2016-03-29 16:45:47 +0200

    Update tests

--- Tests/AppKernel.php
+++ Tests/AppKernel.php
@@ -1,45 +1,69 @@
 <?php
-
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+/**
+ * AppKernel
+ *  
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
 class AppKernel extends Kernel
 {
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\HttpKernel\KernelInterface::registerBundles()
+     */
     public function registerBundles()
     {
-        $bundles = [
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
-        ];
-        
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-        	
-        }
+            
+        	new ASF\DocumentBundle\ASFDocumentBundle()
+        );
 
         return $bundles;
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\HttpKernel\Kernel::getRootDir()
+     */
     public function getRootDir()
     {
         return __DIR__;
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\HttpKernel\Kernel::getCacheDir()
+     */
     public function getCacheDir()
     {
         return __DIR__.'/Fixtures/var/cache/'.$this->getEnvironment();
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\HttpKernel\Kernel::getLogDir()
+     */
     public function getLogDir()
     {
         return __DIR__.'/Fixtures/var/logs';
     }
 
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\HttpKernel\KernelInterface::registerContainerConfiguration()
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $loader->load($this->getRootDir().'/config/config_windows_'.$this->getEnvironment().'.yml');
+            $platform = 'windows';
         } else {
-            $loader->load($this->getRootDir().'/config/config_unix_'.$this->getEnvironment().'.yml');
+            $platform = 'unix';
         }
+        
+        $loader->load($this->getRootDir().'/config/config_'.$platform.'.yml');
     }
 }
